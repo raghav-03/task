6e6c0f96c498e471fe4e3150ff25d5d96fa76ad7
Raghav Agarwal <agarwalraghav892@gmail.com>
2022-06-24 22:17:36 +0530

    Started Task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+// import Data from "./data.json"
+import DataTable from 'react-data-table-component';
+import {  useState } from "react";
+
+const columns = [
+  {
+      name: 'SNo',
+      selector: row => row.sno,
+  },
+  {
+      name: 'Name',
+      selector: row => row.name,
+  },
+  {
+      name: 'DOB',
+      selector: row => row.dob,
+  },
+  {
+      name: 'Age',
+      selector: row => row.age,
+  },
+  {
+      name: 'Occupation',
+      selector: row => row.occupation,
+  },
+  {
+      name: 'Contact No',
+      selector: row => row.contactno,
+  },
+];
+const data = [
+  {
+          name: 'Beetlejuice',
+          dob: '1988',
+      },
+]
+
 
 function App() {
+  const [formdata, setformdata] = useState({
+    name:"",
+    dob:"",
+    age:"",
+    occupation:"",
+    contactno:"",
+  });
+  const changedata=(e)=>{
+    const fieldname=e.target.name;
+    const value=e.target.value;
+    const newformdata={
+      ...formdata,
+    }
+    newformdata[fieldname]=value;
+    setformdata(newformdata);
+  }
+  const adddata=(e)=>{
+    e.preventDefault();
+    formdata.id=1;
+    data.push(formdata);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <DataTable
+            columns={columns}
+            data={data}
+        />
+        <form>
+          <label>Name</label>
+          <input type="text" name="name" value={formdata.name} onChange={changedata}></input>
+          <label>Date of Birth</label>
+          <input type="text" name="dob" value={formdata.dob} onChange={changedata}></input>
+          <label>Age</label>
+          <input type="text" name="age" value={formdata.age} onChange={changedata}></input>
+          <label>Occupation</label>
+          <input type="text" name="occupation" value={formdata.occupation} onChange={changedata}></input>
+          <label>Contact No</label>
+          <input type="text" name="contactno" value={formdata.contactno} onChange={changedata}></input>
+          <button type="submit" onClick={adddata}>Add Data</button>
+        </form>
     </div>
   );
 }
 
 export default App;
+
